19ec56a63de283e6d7485063ced2e224c9298037
Alex Baranov <73061540+SaltyByte@users.noreply.github.com>
2023-03-21 00:14:54 +0200

    123

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import * as moment from 'moment';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly orderService: AppService) {};
-  
+  constructor(private readonly orderService: AppService) {}
+
   getHello(): any {
     throw new Error('Method not implemented.');
   }
@@ -18,26 +19,26 @@ export class AppController {
     @Body('status') status: string,
   ) {
     const newOrderId = await this.orderService.insertOrder(
-      restruantID, 
-      orderID, 
+      restruantID,
+      orderID,
       toppings,
       new Date(date),
-      status
+      status,
     );
     return newOrderId;
   }
 
   @Get()
-  async getOrders(
-    @Query('start') start: string,
-    @Query('end') end: string)
-    {
-      const orders = await this.orderService.getOrderBetween(
-        new Date(start),
-        new Date(end));
-      return { orders }
-    }
-    // example for query: http://localhost:3004/?from=2023-03-16&to=2023-03-18
-    
+  async getOrders(@Query('start') start: string, @Query('end') end: string) {
+    const orders = await this.orderService.getOrderBetween(
+      moment(
+        `${start.split('/')[2]}${start.split('/')[1]}${start.split('/')[0]}`,
+      ).toDate(),
+      moment(
+        `${end.split('/')[2]}${end.split('/')[1]}${end.split('/')[0]}`,
+      ).toDate(),
+    );
+    return { orders };
+  }
+  // example for query: http://localhost:3004/?from=2023-03-16&to=2023-03-18
 }
-      
